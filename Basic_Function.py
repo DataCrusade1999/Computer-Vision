00647e782f4fe2e13042b917b8584d9a3a39feaa
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2021-01-02 18:27:05 +0530

    Inheritance

--- Basic_Function.py
+++ Basic_Function.py
@@ -4,14 +4,22 @@ from Modules.Resize import resized as rs
 
 #BGR Image in Grayscale
 img = cv.imread(r"Photos\Image0.jpg")
-
 img = rs(img,0.40)
-
 img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-
 cv.imshow("Grayscale",img)
 cv.waitKey(0)
 cv.destroyAllWindows()
+
+
+#Blur
+img = cv.imread(r"Photos\Image4.jpg")
+img = rs(img,0.57)
+cv.imshow("Not_Blurred",img)
+blur = cv.GaussianBlur(img,(7,7),cv.BORDER_DEFAULT)
+cv.imshow("Blur",blur)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
 
 
 #Video in Grayscale
@@ -26,4 +34,13 @@ while True:
 
 
 capture.release()
+cv.destroyAllWindows()
+
+
+#Edge Cascade
+img = cv.imread(r"Photos\Image4.jpg")
+img = rs(img,0.57)
+canny = cv.Canny(img,125,175)
+cv.imshow("Edge",canny)
+cv.waitKey(0)
 cv.destroyAllWindows()
