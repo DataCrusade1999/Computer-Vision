f0f4dd2a70b24b699f1384ec4dd874c1c1ab4df9
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2021-01-02 15:40:18 +0530

    added Modules

---
+++ Draw.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+import numpy as np
+import silence_tensorflow.auto
+import tensorflow as tf
+
+
+
+blank = np.array(tf.random.normal([500,500,3]))
+
+cv.imshow("Something",blank)
+
+
+# To color certain portion of the image give blank some range like blank[200:350]
+blank[:]=255,0,0  #BGR format
+cv.imshow("Blue",blank)
+
+
+
+rectangle=cv.rectangle(blank,(0,0),(250,500),(0,255,0),cv.FILLED)
+cv.imshow("Rectangle",rectangle)
+
+
+Circle=cv.circle(blank,(125,125),40,(0,0,255),-1)
+cv.imshow("Circle",Circle)
+
+
+Line=cv.line(blank,(0,0),(blank.shape[1]//2,blank.shape[0]//2),(255,255,255),3)
+cv.imshow("Line",Line)
+
+
+cv.waitKey(0)
+
